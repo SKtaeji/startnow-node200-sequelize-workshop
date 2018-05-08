536df43a87f72206b88dfd6950e43195e1388980
Samuel Kim <caelum.modus@gmail.com>
2018-05-07 17:11:36 -0700

    second commit

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use('/api/authors', require('./db/routes/authors'));
 app.use('/api/blogs', require('./db/routes/blogs'));
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+    res.status(200).send();
 });
 
 module.exports = app;
